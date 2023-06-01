d7c1853c5aec910aad1e968d07bf0cf5e59fbbf5
vernal-spark <arshith1234@gmail.com>
2023-06-01 07:16:30 +0530

    second commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,6 +11,7 @@ const Home = () => {
   return (
     <>
       <Header setOpen={setOpen}/>
+      
       {open&&<Navbar setOpen={setOpen}/>}
       <div className="home-content">
         <div className="home-title">
